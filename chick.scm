2dad69858812f341c50699a7e0a36aab0824e129
Adrian Pistol <vifino@tty.sh>
2018-03-04 21:55:31 +0100

    some more sirc work

--- chick.scm
+++ chick.scm
@@ -4,6 +4,7 @@
 (use inclub)
 (inclub "sirc")
 (import sirc)
+(require-extension matchable)
 
 (define conn
   (sirc:connection "irc.esper.net" nick: "CHICK"))
@@ -16,5 +17,15 @@
 (sirc:join conn "#V")
 
 ;; todo: loop over messages
+(define (mainloop)
+  (let ([parsed (sirc:recieve conn)])
+    (print parsed)
+    (match parsed
+      [(#f "PING" msg) (sirc:send conn "PONG :~A" msg)]
+      [(from "PRIVMSG" to msg) (printf "~A wrote to ~A: ~A" from to msg)]
+      [(from "NOTICE" to msg) (printf "Notice ~A to ~A: ~A" from to msg)]))
+  (mainloop))
 
-(sirc:quit conn "Goodbye.")
+(mainloop)
+
+(sirc:disconnect conn "Goodbye.")
--- sirc.scm
+++ sirc.scm
@@ -2,15 +2,16 @@
 ;; lots stolen from the irc chicken egg
 ;; -*- geiser-scheme-implementation: chicken -*-
 
-(module sirc (sirc:connect sirc:connected? irc:disconnect
+[module sirc (sirc:connect sirc:connected? sirc:disconnect
                            sirc:send sirc:receive
                            sirc:connection-in sirc:connection-out sirc:connection-server sirc:connection-port
                            sirc:connection-nick sirc:connection-user sirc:connection-real sirc:connection? sirc:connection
                            sirc:connection-channels
-                           sirc:join sirc:part
-                           sirc:nick)
+                           sirc:join sirc:part sirc:nick
+                           sirc:msg sirc:notice
+                           sirc:send-ctcp sirc:action)
   (import scheme chicken)
-  (use tcp srfi-1 data-structures regex srfi-13 posix)
+  (use tcp srfi-1 data-structures regex srfi-13 extras posix)
 
 
   (define-record sirc:connection
@@ -35,7 +36,7 @@
      '()))
 
   (define (send con fmt . args)
-    (let ((str (apply sprintf fstr args)))
+    (let ([str (apply sprintf fmt args)])
       (unless (sirc:connected? con)
         (error "not connected" con))
       (fprintf (sirc:connection-out con) "~A\r\n" str)
@@ -43,6 +44,7 @@
 
   (define sirc:send send)
 
+  ;; basic connection management
   (define (sirc:connect con)
     (let-values
         ([(i o) (tcp-connect (sirc:connection-server con) (sirc:connection-port con))])
@@ -62,34 +64,50 @@
     (sirc:connection-in-set! con #f)
     (sirc:connection-out-set! con #f))
 
+  ;; input logic
+  (define (readline con)
+    (read-line (sirc:connection-in con)))
+  (define-constant ircregex (regexp "^(:[^ ]*)? ?([A-Z]+) ([\\w# ]+)?(:.*)?$"))
+  (define (sirc:receive con)
+    (let ([parsed (string-match ircregex (readline con))])
+      (if parsed
+          (cons (second parsed) ; the <optional> prefix
+                (cons (third parsed) ; the command
+                (append (if (fourth parsed)
+                            (string-split (fourth parsed)) ; split the args by space
+                            '())
+                        (if (fifth parsed)
+                            (cons (string-drop (fifth parsed) 1) '()) ; the aftermath
+                            '()))))
+          (error "couldn't parse irc line"))))
+
+  ;; session management
   (define (sirc:join con chan)
-    (let ((chans (lset-adjoin string=? (sirc:connection-channels con) chan)))
+    (let ([chans (lset-adjoin string=? (sirc:connection-channels con) chan)])
       (sirc:connection-channels-set! con chans)
       (send con "JOIN ~A" chan)))
 
   (define (sirc:part con chan)
-    (sirc:connection-channels-set! con
-                                   (delete chan (sirc:connection-channels con) string=?))
+    (sirc:connection-channels-set! con (delete chan (sirc:connection-channels con) string=?))
     (send con "PART ~A" chan))
 
   (define (sirc:nick con nick)
     (sirc:connection-nick-set! con nick)
     (send con "NICK ~A" nick))
 
-  ;; input logic
-  (define (readline con)
-    (read-line (sirc:connection-in con)))
-  (define-constant ircregex (regexp "^([A-Z]+) ([\\w# ]+)?(:.*)?$"))
-  (define (sirc:recv con)
-    (let ((parsed (string-match ircregex (readline con))))
-      (if parsed
-          (cons (second parsed) ; the command
-                (append (if (third parsed)
-                            (string-split (third parsed)) ; split the args by space
-                            '())
-                        (if (fourth parsed)
-                            (cons (string-drop (fourth parsed) 1) '()) ; the aftermath
-                            '())))
-          (error "couldn't parse irc line"))))
+  ;; handy aliases to make things easier
+  (define (sirc:msg con to msg)
+    (send con "PRIVMSG ~A :~A" to msg))
 
-  )
+  (define (sirc:notice con to msg)
+    (send con "NOTICE ~A :~A" to msg))
+
+  (define (sirc:send-ctcp con to type . data)
+    (sirc:msg con to (format "\x01~A\x01"
+                             (if (pair? data)
+                                 (format "~A ~A" type (car data))
+                                 type))))
+  (define (sirc:action con to msg)
+    (sirc:send-ctcp con to "ACTION" msg))
+
+  ]
